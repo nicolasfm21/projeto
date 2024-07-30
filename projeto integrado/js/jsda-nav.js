0bb30eb1ac4a79b3948b3a9c02a8a037e49ed055
nicolasfm21 <nicolas.freire.oficial2006@gmail.com>
2024-07-29 21:03:02 -0300

    projeto integrado

---
+++ projeto integrado/js/jsda-nav.js
@@ -0,0 +1,122 @@
+function ativarElemento(elemento) {
+    elemento.classList.remove("item");
+    
+  }
+
+
+ 
+
+  function mostrarTexto1(texto) {
+    
+    document.getElementById('info1').style.display ="flex";
+
+
+ 
+    document.getElementById('info2').style.display ="none";
+    
+  
+  
+    document.getElementById('info3').style.display ="none";
+    
+ 
+ 
+    document.getElementById('info4').style.display ="none";
+    
+
+  
+    document.getElementById('info5').style.display ="none";
+ 
+  
+  }
+
+  function mostrarTexto2(texto) {
+
+    document.getElementById('info1').style.display ="none";
+
+
+ 
+    document.getElementById('info2').style.display ="flex";
+    
+  
+  
+    document.getElementById('info3').style.display ="none";
+    
+ 
+ 
+    document.getElementById('info4').style.display ="none";
+    
+
+  
+    document.getElementById('info5').style.display ="none";
+ 
+  
+  }
+
+  function mostrarTexto3(texto) {
+
+    document.getElementById('info1').style.display ="none";
+
+
+ 
+    document.getElementById('info2').style.display ="none";
+    
+  
+  
+    document.getElementById('info3').style.display ="flex";
+    
+ 
+ 
+    document.getElementById('info4').style.display ="none";
+    
+
+  
+    document.getElementById('info5').style.display ="none";
+ 
+  
+  }
+
+  function mostrarTexto4(texto) {
+
+    document.getElementById('info1').style.display ="none";
+
+
+ 
+    document.getElementById('info2').style.display ="none";
+    
+  
+  
+    document.getElementById('info3').style.display ="none";
+    
+ 
+ 
+    document.getElementById('info4').style.display ="flex";
+    
+
+  
+    document.getElementById('info5').style.display ="none";
+ 
+  
+  }
+
+  function mostrarTexto5(texto) {
+
+    document.getElementById('info1').style.display ="none";
+
+
+ 
+    document.getElementById('info2').style.display ="none";
+    
+  
+  
+    document.getElementById('info3').style.display ="none";
+    
+ 
+ 
+    document.getElementById('info4').style.display ="none";
+    
+
+  
+    document.getElementById('info5').style.display ="flex";
+ 
+  
+  }
